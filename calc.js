7ef853b45839c5facd46791e9a2cad2c59cf3a61
Steven Martin <83900183+StevenM27@users.noreply.github.com>
2024-06-15 16:34:46 -0700

    Create basic functionality. Still requires debugging for long strings of operations.

--- calc.js
+++ calc.js
@@ -1,6 +1,6 @@
 let num1 = 0;
 let num2 = 0;
-let operator = "";
+let operatorValue = "";
 let displayValue = "0";
 
 function add(a, b) {
@@ -65,3 +65,41 @@ numberButtons.forEach(button => {
         display.textContent = displayValue;
     });
 })
+
+const clearButton = document.querySelector("#clear");
+clearButton.addEventListener("click", () => {
+    displayValue = "0";
+    num1 = 0;
+    num2 = 0;
+    operatorValue = "";
+    display.textContent = displayValue;
+});
+
+const operatorButtons = document.querySelectorAll(".operator");
+operatorButtons.forEach(operator => {
+    operator.addEventListener("click", () => {
+        if (operatorValue === "") {
+            num1 = parseInt(displayValue);
+            operatorValue = operator.id;
+            displayValue = "0";
+        }
+        else {
+            num2 = parseInt(displayValue);
+
+            num1 = operate(operatorValue, num1, num2);
+            displayValue = num1 + "";
+            display.textContent = displayValue;
+            operatorValue = "";
+        }
+    });
+});
+
+const equalsButton = document.querySelector("#equals");
+equalsButton.addEventListener("click", () => {
+    num2 = parseInt(displayValue);
+
+    const solution = operate(operatorValue, num1, num2);
+    displayValue = solution + "";
+    display.textContent = displayValue;
+    operatorValue = "";
+});
